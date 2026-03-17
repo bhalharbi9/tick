f1f8a584a63cc38e2bd3276399d96c086a968983
bhalharbi9 <bhalharbi8@gmail.com>
2026-03-17 20:31:58 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://YOUR-PROJECT.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_ANON_KEY",
+  SUPABASE_URL: "https://wgyynddbnuamgmuwszxs.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_iq58utkapaJzH0q-555vZA_n0pxI9QA",
   ADMIN_PASS: "123456",
   AUTO_REFRESH_MS: 15000
 };
