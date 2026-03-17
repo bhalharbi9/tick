f83c7133674249b58c5b1430a5b6ba318ad6a103
bhalharbi9 <bhalharbi8@gmail.com>
2026-03-17 21:01:45 +0300

    Change Supabase ANON_KEY in config.js
    Updated the Supabase anonymous key for security.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://wgyynddbnuamgmuwszxs.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndneXluZGRibnVhbWdtdXdzenhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3NjM5ODYsImV4cCI6MjA4OTMzOTk4Nn0.739BpQg37HuP31fKr3x3oJCDJAEyl1-RvIGuBEmcsrQ",
+  SUPABASE_ANON_KEY: "sb_publishable_iq58utkapaJzH0q-555vZA_n0pxI9QA",
   ADMIN_PASS: "N852673b",
   AUTO_REFRESH_MS: 15000
 };
