9a041fa84c972ba84356926306d5a6b6f32dca32
bhalharbi9 <bhalharbi8@gmail.com>
2026-03-17 21:49:36 +0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,6 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://wgyynddbnuamgmuwszxs.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_iq58utkapaJzH0q-555vZA_n0pxI9QA",
-  ADMIN_PASS: "N852673b",
-  AUTO_REFRESH_MS: 15000
-};
